4da5fec8c464cfe1bd4045707559955bde608ac5
IbragimDodov <utisey@gmail.com>
2022-11-11 23:56:44 +0300

    adaptive 960/768/320

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,8 @@
 
 const range = document.getElementById('myRange');
 const rangeOutput = document.getElementById('range-output-input');
+const burgerBtn = document.querySelector('.burger-btn');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -9,27 +11,87 @@ document.addEventListener("DOMContentLoaded", () => {
     rangeOutput.value = range.value + ' литров';
   }
 
+  burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('opened');
+    burgerBtn.classList.contains('opened') ? burgerBtn.setAttribute('data','aria-expanded') : burgerBtn.removeAttribute('data');
+    mobileMenu.classList.toggle('d-block');
+
+    if (mobileMenu.classList.contains("d-block")) {
+      mobileMenu.style.display = "block";
+    } else if (!mobileMenu.classList.contains("d-block")) {
+      mobileMenu.style.display = "none";
+    }
+  })
+
 
   $('.certificates__bottom').slick({
     slidesToShow: 4,
-    adaptiveHeight: true,
+    slidesToScroll: 1,
     prevArrow: '<button type="button" class="slick-prev"><img src="dist/img/icons/arrow-prev.svg" alt="arrow-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="dist/img/icons/arrow-next.svg" alt="arrow-next"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="dist/img/icons/arrow-next.svg" alt="arrow-next"></button>',
+    responsive: [
+      {
+        breakpoint: 961,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 321,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('.reviews__body').slick({
     slidesToShow: 2,
-    adaptiveHeight: true,
     prevArrow: '<button type="button" class="slick-prev"><img src="dist/img/icons/arrow-prev.svg" alt="arrow-prev"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="dist/img/icons/arrow-next.svg" alt="arrow-next"></button>'
+    nextArrow: '<button type="button" class="slick-next"><img src="dist/img/icons/arrow-next.svg" alt="arrow-next"></button>',
+    responsive: [
+      {
+        breakpoint: 961,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 321,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 
   $('.partners__items').slick({
     slidesToShow: 4,
-    adaptiveHeight: true,
     dots: true,
     nextArrow: false,
-    prevArrow: false
+    prevArrow: false,
+    responsive: [
+      {
+        breakpoint: 961,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 321,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
+        }
+      }
+    ]
   });
   
 })
